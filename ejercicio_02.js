066e0c11d396f04308909247d822d72e87554e43
jose.mejia <desarrollo@fmsupplyhn.com>
2026-06-09 08:48:31 -0600

    ejercicio 2, documentacion inicial

---
+++ ejercicio_02.js
@@ -0,0 +1,21 @@
+/* 
+*Ejercicio 2: Simulación de una Operación Asíncrona con Promesas 
+
+?Descripción: 
+Cree una función que simule una operación asíncrona, como una consulta a una 
+base de datos, que tarda 3 segundos en completarse. La función debe devolver una 
+promesa que se resuelve con un mensaje de éxito o se rechaza con un mensaje de 
+error basado en una condición aleatoria. 
+
+?Instrucciones: 
+1. Define una función consultarBaseDeDatos() que devuelva una promesa. 
+2. Dentro de la función, Utilice setTimeout para simular un retraso de 3 
+segundos. 
+3. Genera un número aleatorio para decidir si la promesa se resuelve o se 
+rechace . 
+4. Si el número es par, resuelve la promesa con el mensaje "Consulta exitosa". 
+5. Si el número es impar, rechace  la promesa con el mensaje "Error en la 
+consulta". 
+6. Llama a la función y maneja el resultado con .then() y .catch().
+
+*/
