1b86d406a09e03094c2217925d96ef2838e897e0
jose.mejia <desarrollo@fmsupplyhn.com>
2026-06-09 09:01:58 -0600

    ejercicio 3, documentacion inicial

---
+++ ejercicio_03.js
@@ -0,0 +1,18 @@
+/* 
+*Ejercicio 3: Uso de async y await para Manejar Promesas 
+
+?Descripción: 
+Reescribe el ejercicio anterior Utilizando async y await en lugar de .then() y .catch() 
+para manejar la promesa devuelta por consultarBaseDeDatos(). 
+
+?Instrucciones: 
+1. Cree una función ejecutarConsulta() declarada con async. 
+2. Dentro de la función, Utilice await para llamar a consultarBaseDeDatos(). 
+3. Maneja la resolución y el recHagao de la promesa Utilizando try...catch. 
+4. Llama a ejecutarConsulta() para ejecutar el código.
+
+*/
+
+
+
+
